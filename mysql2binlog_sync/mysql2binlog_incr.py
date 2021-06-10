be04a431fb3e39655fb4a6e16955e7e20c02e2cf
mafei <zhdn_791005@163.com>
2021-06-10 17:27:23 +0800

    binlog sync update

--- mysql2binlog_sync/mysql2binlog_incr.py
+++ mysql2binlog_sync/mysql2binlog_incr.py
@@ -46,10 +46,10 @@ def get_event_name(event):
 
 
 MYSQL_SETTINGS = {
-    "host"  : "10.2.39.18",
+    "host"  : "10.2.39.240",
     "port"  : 3306,
-    "user"  : "puppet",
-    "passwd": "Puppet@123",
+    "user"  : "canal",
+    "passwd": "canal@Hopson2020",
     "db"    : "test"
 }
 
@@ -60,11 +60,6 @@ MYSQL_SYNC_SETTINGS = {
     "passwd": "Puppet@123",
     "db"    : "sync"
 }
-
-def get_defi_pos(file=None,pos=None):
-   file = 'sync-mysql-bin.000019'
-   pos ='451310173'
-   return file,pos
 
 def get_master_pos(file=None,pos=None):
     db = get_db(MYSQL_SETTINGS)
@@ -129,86 +124,116 @@ def gen_sql(event):
        pass
     return sql
 
+
+def get_binlog(p_file,p_db,p_tab,p_start_pos,p_end_pos):
+    try:
+        stream = BinLogStreamReader(connection_settings=MYSQL_SETTINGS,
+                                    only_schemas=[p_db],
+                                    only_tables=[p_tab],
+                                    only_events=(QueryEvent, DeleteRowsEvent, UpdateRowsEvent, WriteRowsEvent),
+                                    server_id=8,
+                                    blocking=True,
+                                    resume_stream=True,
+                                    log_file=p_file,
+                                    log_pos=int(p_start_pos)
+                                    )
+
+        schema = MYSQL_SETTINGS['db']
+
+        for binlogevent in stream:
+            print('stream.log_file,stream.log_pos=', stream.log_file, stream.log_pos)
+
+            if stream.log_pos >= p_end_pos:
+                stream.close()
+                sys.exit(0)
+
+            if binlogevent.event_type in (2,):
+
+                event = {"schema": bytes.decode(binlogevent.schema), "query": binlogevent.query.lower()}
+
+                if 'create' in event['query'] or 'drop' in event['query'] \
+                        or 'alter' in event['query'] or 'truncate' in event['query']:
+
+                    if event['schema'] == schema:
+                        print(binlogevent.query.lower())
+
+
+            if binlogevent.event_type in (30, 31, 32):
+                # print('binlogevent=',binlogevent.dump())
+                print('binlogevent.packet.log_pos=', binlogevent.packet.log_pos)
+
+                for row in binlogevent.rows:
+                    # print('binlogevent.packet.log_pos=', binlogevent.packet.log_pos)
+                    # print('stream.log_file,stream.log_pos=', stream.log_file, stream.log_pos)
+
+                    event = {"schema": binlogevent.schema, "table": binlogevent.table}
+
+                    if event['schema'] == schema:
+
+                        if isinstance(binlogevent, DeleteRowsEvent):
+                            event["action"] = "delete"
+                            event["data"] = row["values"]
+                            print('delete=', gen_sql(event), event)
+
+                        elif isinstance(binlogevent, UpdateRowsEvent):
+                            event["action"] = "update"
+                            event["after_values"] = row["after_values"]
+                            event["before_values"] = row["before_values"]
+                            print('update=', gen_sql(event), event)
+
+                        elif isinstance(binlogevent, WriteRowsEvent):
+                            event["action"] = "insert"
+                            event["data"] = row["values"]
+                            print('insert=', gen_sql(event), event)
+
+                        # print(get_event_name(binlogevent.event_type),json.dumps(event, cls=DateEncoder))
+                        # print(json.dumps(event))
+
+
+
+    except Exception as e:
+        print(str(e))
+    finally:
+        stream.close()
+
+def exec_sql(p_db,p_tab,p_sql):
+    db = get_db(MYSQL_SETTINGS)
+    cr = db.cursor()
+    # 1.获取 file,start_pos
+    cr.execute('FLUSH /*!40101 LOCAL */ TABLES')
+    cr.execute('FLUSH TABLES WITH READ LOCK')
+    cr.execute('show master status')
+    rs=cr.fetchone()
+    file = rs[0]
+    start_pos= rs[1]
+    cr.execute('unlock tables')
+
+    # 2.执行SQL语句
+    print('use {}'.format(p_db))
+    cr.execute('use {}'.format(p_db))
+    print('lock tables {} write'.format(p_tab))
+    cr.execute('lock tables {} write'.format(p_tab))
+    print(p_sql)
+    cr.execute(p_sql)
+
+    # 3.获取 stop_position
+    cr.execute('show master status')
+    rs = cr.fetchone()
+    stop_pos = rs[1]
+    cr.execute('unlock tables')
+
+    return file,start_pos,stop_pos
+
+
+
 def main():
-    # while True:
-        try:
-            # file,pos=get_master_pos()
-            file,pos=get_defi_pos()
-            stream = BinLogStreamReader( connection_settings = MYSQL_SETTINGS,
-                                         only_schemas=["test"],
-                                         only_tables=["xs"],
-                                         only_events=(QueryEvent,DeleteRowsEvent,UpdateRowsEvent,WriteRowsEvent),
-                                         server_id     = 8,
-                                         blocking      = True,
-                                         resume_stream = True,
-                                         log_file = file,
-                                         log_pos  = int(pos)
-                                        )
+    db = 'test'
+    tab ='xs'
+    sql = """update xs set `name`='liuluoquo2' where xh=2"""
+    file,start_pos,stop_pos = exec_sql(db,tab,sql)
+    print('parameter:',db,tab,file,start_pos,stop_pos)
 
-            schema = MYSQL_SETTINGS['db']
-
-            # event = stream.fetchone()
-            # print(event.table)
-
-            for binlogevent in stream:
-
-                # print('stream.log_file,stream.log_pos=', stream.log_file, stream.log_pos)
-
-                if binlogevent.event_type in (2,):
-
-                    event = {"schema": bytes.decode(binlogevent.schema), "query": binlogevent.query.lower()}
-
-                    if 'create' in event['query'] or 'drop' in event['query'] \
-                            or 'alter' in event['query'] or 'truncate' in event['query']:
-
-                        if event['schema'] == schema:
-                            #print('event2=>',get_event_name(binlogevent.event_type), event)
-                            #print('binlogevent.packet.log_pos=', binlogevent.packet.log_pos)
-                            print(binlogevent.query.lower())
-                            print('stream.log_file,stream.log_pos=',stream.log_file,stream.log_pos)
-
-
-                if binlogevent.event_type in (30, 31, 32):
-
-                    #print('binlogevent=',binlogevent.dump())
-                    print('binlogevent.packet.log_pos=',  binlogevent.packet.log_pos)
-
-                    for row in binlogevent.rows:
-                        #print('binlogevent.packet.log_pos=', binlogevent.packet.log_pos)
-                        # print('stream.log_file,stream.log_pos=', stream.log_file, stream.log_pos)
-
-                        event = {"schema": binlogevent.schema, "table": binlogevent.table}
-
-                        if event['schema'] == schema:
-
-                            if isinstance(binlogevent, DeleteRowsEvent):
-                                event["action"] = "delete"
-                                event["data"] = row["values"]
-                                print('delete=', gen_sql(event),event)
-
-                            elif isinstance(binlogevent, UpdateRowsEvent):
-                                event["action"] = "update"
-                                event["after_values"] = row["after_values"]
-                                event["before_values"] = row["before_values"]
-                                print('update=', gen_sql(event),event)
-
-                            elif isinstance(binlogevent, WriteRowsEvent):
-                                event["action"] = "insert"
-                                event["data"] = row["values"]
-                                print('insert=', gen_sql(event),event)
-
-                            #print(get_event_name(binlogevent.event_type),json.dumps(event, cls=DateEncoder))
-                            #print(json.dumps(event))
-
-
-                if stream.log_pos == 455886697:
-                    break
-
-        except Exception as e:
-            print(str(e))
-        finally:
-            stream.close()
-
+    get_binlog(file, db, tab, start_pos, stop_pos)
 
 if __name__ == "__main__":
     main()
